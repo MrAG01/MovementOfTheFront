181f8b538b5d9c6ca75c95b0e23bc2b81f78d22a
MrAG <andrryy2004@inbox.ru>
2025-12-01 15:14:22 +0300

    Начал делать систему модов

--- components/building/building_config.py
+++ components/building/building_config.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from components.bullets.base_bullet import BaseBullet
 
 
 @dataclass
@@ -6,30 +7,39 @@ class BuildingConfig:
     name: str
     description: str
     cost: dict[str, int]
-    build_time: float
-    max_level: int
-    next_worker_cost_multiplier: float
-    next_building_cost_multiplier: float
-    max_cost: dict[str, int]
-    requirements_buildings: list[str]
-    requirements_technologies: list[str]
-    max_health: float
-    regeneration: float
-    conducts_roads: bool
-    size: tuple[int, int]
+    build_time: float = 1.0
+    max_level: int = 1
+    next_worker_cost_multiplier: float = 1.0
+    next_building_cost_multiplier: float = 1.0
+    max_cost: dict[str, int] = None
+    requirements_buildings: list[str] = None
+    requirements_technologies: list[str] = None
+    max_health: float = 100.0
+    regeneration: float = 1.0
+    conducts_roads: bool = False
+    size: tuple[int, int] = (50, 50)
 
     # Производство (опционально)
-    production: dict[str, int]
-    consumption: dict[str, int]
-    workers_slots: int
-    production_speed: float
+    production: dict[str, int] = None
+    consumption: dict[str, int] = None
+    workers_slots: int = 5
+    production_speed: float = 1.0
 
     # Оборона (опционально)
+    reload_time: float = 1.0
+    attack_radius: float = 100.0
+    turret_rotate_speed: float = 5.0
+    bullet: BaseBullet = None
+    can_target_projectiles = False
+    can_target_air = False
+    can_target_ground = True
+    can_target_water = True
+    can_target_buildings = True
 
+    base_texture_name: str = None
+    texture_name: str = None
+    turret_texture_name: str = None
+    building_animation_name: str = None
 
-    base_texture_name: str
-    texture_name: str
-    building_animation_name: str
-
-    building_sound_name: str
-    working_sound_name: str
+    building_sound_name: str = None
+    working_sound_name: str = None
---
+++ resources/data_loader.py
@@ -0,0 +1,55 @@
+import json
+from dataclasses import dataclass, field
+
+
+@dataclass
+class ModMetaData:
+    name: str
+    priority: int
+    author: str
+    version: str
+    game_version: str
+    description: str = ""
+    dependencies: list[str] = field(default_factory=list)
+
+    def __init__(self, path):
+        with open(path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+        self.name = data.get("name", "Unnamed Mod")
+        self.priority = data.get("priority", 0)
+        self.author = data.get("author", "Unknown")
+        self.version = data.get("version", "1.0.0")
+        self.game_version = data.get("game_version")
+        self.description = data.get("description", "")
+        self.dependencies = data.get("dependencies", [])
+
+
+class ModData:
+    def __init__(self, path):
+        self._loaded = False
+        self.buildings = {}
+        self.units = {}
+
+    def load(self):
+        pass
+
+
+class Mod:
+    def __init__(self, path):
+        self.base_path = path
+        self.meta_data = ModMetaData(f"{path}/mod.json")
+        self.mod_data = ModData(path)
+
+
+class DataLoader:
+    def __init__(self, config_manager, default_data_path, mod_data_path):
+        self.config_manager = config_manager
+        self.default_data_path = default_data_path
+        self.mod_data_path = mod_data_path
+        self.true_data = Mod(self.default_data_path)
+
+        # name, path
+        self.available_mods: dict[str, str] = {}
+
+    def _scan_mod_folder(self):
+        pass
