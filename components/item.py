41a6ca4936ea1560d6bdf7bfeeb81eb8d4912c8f
MrAG <andrryy2004@inbox.ru>
2025-12-09 03:26:19 +0300

    Продолжил делать сервер и клиента. Реализовал класс Item.

---
+++ components/item.py
@@ -0,0 +1,89 @@
+class Item:
+    def __init__(self, item_type, amount):
+        self.item_type = item_type
+        self.amount = amount
+
+    def move(self, other, amount):
+        if other.item_type != self.item_type:
+            raise TypeError
+
+        move_amount = min(amount, self.amount)
+        self.amount -= move_amount
+        other.amount += move_amount
+
+    def add(self, amount):
+        self.amount += amount
+
+    def remove(self, amount):
+        if self.amount >= amount:
+            self.amount -= amount
+            return True
+        else:
+            return False
+
+    def has_amount(self, amount):
+        return self.amount >= amount
+
+    def set_amount(self, amount):
+        self.amount = amount
+
+    def copy(self):
+        return Item(self.item_type, self.amount)
+
+    def __lt__(self, other):
+        if isinstance(other, Item):
+            return self.amount < other.amount
+        elif isinstance(other, int) or isinstance(other, float):
+            return self.amount < other
+        else:
+            return False
+
+    def __eq__(self, other):
+        if isinstance(other, Item):
+            return self.amount == other.amount and self.item_type == other.item_type
+        elif isinstance(other, int) or isinstance(other, float):
+            return self.amount == other
+        else:
+            return False
+
+    def __add__(self, other):
+        if isinstance(other, Item):
+            if self.item_type != other.item_type:
+                raise TypeError
+            return Item(self.item_type, self.amount + other.amount)
+        elif isinstance(other, int):
+            return Item(self.item_type, self.amount + other)
+        else:
+            raise TypeError
+
+    def __sub__(self, other):
+        if isinstance(other, Item):
+            if self.item_type != other.item_type:
+                raise TypeError
+            return Item(self.item_type, max(self.amount - other.amount, 0))
+        elif isinstance(other, int):
+            return Item(self.item_type, max(self.amount - other, 0))
+        else:
+            raise TypeError
+
+    def __iadd__(self, other):
+        if isinstance(other, Item):
+            if self.item_type != other.item_type:
+                raise TypeError
+            self.amount += other.amount
+            other.amount = 0
+        elif isinstance(other, int):
+            self.amount += other
+        else:
+            raise TypeError
+        return self
+
+    def serialize(self):
+        return {
+            "item_type": self.item_type,
+            "amount": self.amount
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**data)
--- network/server/game_server.py
+++ network/server/game_server.py
@@ -1,7 +1,25 @@
+import time
 from threading import Thread
+
+from game.game_state import GameState
 from network.server.game_server_config import GameServerConfig
 from core.callback import Callback
 import socket
+
+
+class ClientHandler:
+    def __init__(self, client_id, client_socket, address, on_command, on_disconnect):
+        self.client_id = client_id
+        self.client_socket = client_socket
+        self.address = address
+        self.on_command = on_command
+        self.on_disconnect = on_disconnect
+
+    def run(self):
+        pass
+
+    def on_shutdown(self):
+        pass
 
 
 class GameServer:
@@ -11,6 +29,10 @@ class GameServer:
         self.server_socket: socket.socket = None
         self._client_threads = []
         self._running = False
+
+        self.clients = {}
+        self.game_state = GameState()
+        self.next_player_id = 1
 
     def add_listener(self, callback):
         self.listeners_callbacks.add(callback)
@@ -55,7 +77,23 @@ class GameServer:
         self.server_socket.listen()
 
     def _handle_client(self, client_socket, address):
+        handler = ClientHandler(
+            client_id=self.next_player_id,
+            client_socket=client_socket,
+            address=address,
+            on_command=self._handle_player_command,
+            on_disconnect=self._handle_player_disconnect
+        )
+        self.clients[self.next_player_id] = handler
+        self.next_player_id += 1
+        handler.run()
+
+    def _handle_player_command(self, client_handler, command):
         pass
+
+    def _handle_player_disconnect(self, client_id):
+        self.clients[client_id].on_shutdown()
+        del self.clients[client_id]
 
     def _start_handle_client_thread(self, client_socket, address):
         if len(self._client_threads) >= self.get_max_players():
@@ -66,6 +104,32 @@ class GameServer:
         client_thread = Thread(target=self._handle_client, args=(client_socket, address), daemon=True)
         client_thread.start()
         self._client_threads.append(client_thread)
+
+    def _process_commands(self):
+        pass
+
+    def _update_game_state(self):
+        pass
+
+    def _send_snapshots(self):
+        pass
+
+    def game_loop(self):
+        tick_rate = 20
+        tick_duration = 1.0 / tick_rate
+        while self._running:
+            start_time = time.time()
+
+            self._process_commands()
+            self._update_game_state()
+            self._send_snapshots()
+
+            end_time = time.time()
+            sleep_time = max(0.0, tick_duration - (end_time - start_time))
+            time.sleep(sleep_time)
+
+            self.game_state.tick += 1
+            self.game_state.time = end_time
 
     def _main_cycle(self):
         self._send_message_to_listeners(Callback.ok("Waiting for connections..."))
